f3149c3d77e72b71d1ae8b749afb7c6adab486d5
Henry Diaz <henrydiazlds123@hotmail.com>
2014-06-21 18:10:15 -0400

    Java Beans Serializable

--- FinalGame/src/finalgame/Game.java
+++ FinalGame/src/finalgame/Game.java
@@ -6,21 +6,81 @@
 package finalgame;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
  * @author henrydiazlds
  */
-public class Game {
+public class Game implements java.io.Serializable {
+
+    private int gamesPlayed = 0;
+    private int gamesWonR   = 0;
+    private int gamesWonB   = 0;
+    private int tieCounter  = 0;
+    
+
+    String choose = null;
+    String invalidEntry = "Wrong Entry, Please try again.\n";
+
+    boolean valid = true;
 
     Game() {
+        
+    }
 
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(int gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
+    public int getGamesWonR() {
+        return gamesWonR;
+    }
+
+    public void setGamesWonR(int gamesWonR) {
+        this.gamesWonR = gamesWonR;
+    }
+
+    public int getGamesWonB() {
+        return gamesWonB;
+    }
+
+    public void setGamesWonB(int gamesWonB) {
+        this.gamesWonB = gamesWonB;
+    }
+
+    public int getTieCounter() {
+        return tieCounter;
+    }
+
+    public void setTieCounter(int tieCounter) {
+        this.tieCounter = tieCounter;
+    }
+
+    public String getChoose() {
+        return choose;
+    }
+
+    public void setChoose(String choose) {
+        this.choose = choose;
+    }
+
+    public String getInvalidEntry() {
+        return invalidEntry;
+    }
+
+    public void setInvalidEntry(String invalidEntry) {
+        this.invalidEntry = invalidEntry;
     }
 
     public static void main(String[] args) throws IOException {
         Interfase interfase = new Interfase();
         interfase.getName();
-
+        new Game();
     }
-
 }
