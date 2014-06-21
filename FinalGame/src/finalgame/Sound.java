a79729f2a3bd8c73e1f2836d98175f6238e47a5f
Henry Diaz <henrydiazlds123@hotmail.com>
2014-06-21 15:53:08 -0400

    Sound Class with Methods

--- FinalGame/src/finalgame/Sound.java
+++ FinalGame/src/finalgame/Sound.java
@@ -3,13 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package finalgame;
 
 /**
  *
  * @author henrydiazlds
  */
+import java.applet.Applet;
+import java.applet.AudioClip;
+
 public class Sound {
-    
+
+    public static AudioClip CHIP     = Applet.newAudioClip(Sound.class.getResource("ball.wav"));
+    public static AudioClip GAMEOVER = Applet.newAudioClip(Sound.class.getResource("gameover.wav"));
+    public static AudioClip BACK     = Applet.newAudioClip(Sound.class.getResource("back.wav"));
+    //public static AudioClip WRONG = Applet.newAudioClip(Sound.class.getResource("wrong.wav"));
+
+    public static void playMusic() {
+        BACK.loop();
+    }
+
+    public static void stopMusic() {
+        GAMEOVER.play();
+        BACK.stop();
+    }
 }
