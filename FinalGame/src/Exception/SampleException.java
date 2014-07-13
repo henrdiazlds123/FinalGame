30d7684e013919f759a5761a51d49803055c7f5b
kylugo <kylugo@192.168.2.3>
2014-07-13 11:41:41 -0600

    Merge origin/master

--- FinalGame/src/Exception/SampleException.java
+++ FinalGame/src/Exception/SampleException.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Exception;
 
 /**
@@ -11,9 +10,31 @@ package Exception;
  * @author Piedad
  */
 public class SampleException {
-    SampleException(Exception ex){
-    
-    }
 
-   
+    public static void main(String Args[]) {
+
+        int[] error = new int[3];
+        try {
+
+            for (int e = 0; e < 4; ++e) {
+
+                error[e] = e;
+
+            }
+
+            System.out.println(error);
+
+        } catch (ArrayIndexOutOfBoundsException ex) {
+
+            
+            System.out.println("Sorry, that's an error!");
+
+        } finally {
+
+            System.out.println(error);
+
+            
+        }
+
+    }
 }
